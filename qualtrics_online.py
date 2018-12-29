2ce51a34b9cb2a697fd813898b80d7e25cfbc320
callumnelson95 <callum_nelson@brown.edu>
2018-12-29 17:53:02 -0500

    Still working on ID checking

--- qualtrics_online.py
+++ qualtrics_online.py
@@ -208,7 +208,18 @@ def normalize_crosstab(values, program, year, day):
 				re.compile('learning objectives'): ['Objectives', 'Please indicate to what extent these learning objectives were effectively covered during the course'],
 				re.compile('support.*staff'): ['Support', 'Please indicate your satisfaction with the support from the program staff']
 				},
-		'DWAU': [],
+		'DWAU': {re.compile('rate the overall quality'): ['Quality', 'How would you rate the overall quality of this program?'],
+				re.compile('equity is central'): ['Equity1', "Today's sessions helped me articulate how and why equity is central to the work of school improvement"],
+				re.compile('service of equity'): ['Equity2',"Today's class and activities helped me reflect on how Data Wise tools could be used in the service of equity"],
+				re.compile('team.*norms'): ['Team Norms',"In the team that you came to Harvard with, how well are you and your colleagues following norms?"],
+				re.compile('group.*norms'): ['Group Norms', "To what extent did your case group practice our Data Wise norms today?"],
+				re.compile('professionally useful'): ['Useful', "To what extent did you find the Data Wise course professionally useful?"],
+				re.compile('modify your professional practice'): ['Modify', "How much do you intend to modify your professional practice, based on your experience in the Data Wise course?"],
+				re.compile("diversity of.*learning.*community"): ['Diversity', "How satisfied were you with the diversity of the course's learning community, inclusive of racial, ethnic, professional, personal, regional, institution type, and other perspectives and backgrounds?"],
+				re.compile('scale of 0 to 10'): ['Recommend', 'On a scale of 0 to 10, how likely is it that you would recommend the Data Wise Leadership Institute to a friend or colleague?'],
+				re.compile('testimonial'): ['Testimonial', 'Please use the space below to share your testimonial'],
+				re.compile('support.*staff'): ['Support', 'Please indicate your satisfaction with the support from the program staff']
+				},
 		'DWN': [],
 
 	}
--- write_to_csv.py
+++ write_to_csv.py
@@ -4,13 +4,13 @@ from sys import argv
 
 
 
-def main(program, year, day):
+def main(program, year, day, survey_id):
 
 	with open('public/data/uploaded_surveys.csv', 'a') as csvfile:
 	    writer = csv.writer(csvfile)
-	    writer.writerow([program, year, day])
+	    writer.writerow([program, year, day, survey_id])
 
 
 if __name__ == '__main__':
-	script, program, year, day = argv
-	main(program, year, day)
+	script, program, year, day, survey_id = argv
+	main(program, year, day, survey_id)
